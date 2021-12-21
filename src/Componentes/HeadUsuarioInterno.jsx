8cbea0dc1ffe4f598a28ceecde275f7647d837ed
Vides93 <videsda@uninorte.edu.com>
2021-12-21 16:49:32 -0500

    cambio del logo

--- src/Componentes/HeadUsuarioInterno.jsx
+++ src/Componentes/HeadUsuarioInterno.jsx
@@ -35,7 +35,7 @@ export const HeadUsuarioInterno = () => {
             </li>
             <li className="pull-rigth">
               <img
-                src="https://s3-alpha-sig.figma.com/img/f323/cb54/fc0c457aacd809fb5b7bacc2a6a808d0?Expires=1639353600&Signature=g5vLxOSBwHiXfK~K1gYgZ1tmTB5V3bjRoEnKeqMKg1UkhJmKve02oUBnWyOURpQgb1zWgpgpdE0MLJ2hjbNKyha-YcFA3BwhWcRpYKPCby2~GddhtB5DVPeapdQGLAf9NkGA1I-dQHx9BSskryBZjsg4Xx~ubFd7HsDTkQf9rbYrxwnnRPUVs1eZucZpNjy34iNMfnHS04VQWiNa~eUB1k8F5vp9jPr4D0J0UaaOm52wG~SGTI-TfVYTyFziG0QrcmoDE15hUHy~CYwyCO4cCHVOQAjapEea2qRYz~DgiPU4jza6YWrhEeoTHow2EGNe5DWFrpqsgJQFjaF-XKolBw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                src="https://s3-alpha-sig.figma.com/img/f323/cb54/fc0c457aacd809fb5b7bacc2a6a808d0?Expires=1641168000&Signature=aHhBIqQNI5xQQCJwOnQpl9SFJxpxJ4XwUNwpYA4WLOJSoEMafsbf66-h1Ry3G~tBQ5MxvbRpihk1I83wu216-GYlyrpYsmn211VO2cIr3OM9RNSYMxCMx~fvaoPauPVuKlJ60~uFLTXwlBLnLCPX06t5Ivn4Skc~jgS8u9ZzkmgWVGOn6db2SAZ9AmzsomJ6eTThiV-JphAHw-LioDwWBg58aeo~TLUuaZ-tdCwfMKjrm22LQ-R2fMQSrACBKtkUcLYjAUjixNFP0aTaxf7N7t4oyrViWt5Vmb1oVZJ~Tq0vpeNQ-aF6vOa7ZfAeHpBSdyvaFBGZ2zS9jjFVUuhYUw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
                 alt=""
                 width="140"
                 height="125"
